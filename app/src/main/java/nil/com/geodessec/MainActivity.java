c2a29969b4fd5b6acf263f4027f2cbdd8424af32
NileshArnaiya <nileshjain61@yahoo.in>
2019-01-03 03:00:03 +0530

    added range seekbar with start and end points

--- app/src/main/java/nil/com/geodessec/MainActivity.java
+++ app/src/main/java/nil/com/geodessec/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.media.audiofx.Visualizer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,6 +18,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.emrekose.recordbutton.RecordButton;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -26,6 +26,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,25 +36,38 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener {
 
-    private MediaRecorder mRecorder;
     public static final String TAG = "MainActivity";
-    private ImageView recordButton;
+    private static String mFileName = null;
+    PermissionListener permissionlistener;
+    Runnable r;
+    private MediaRecorder mRecorder;
+    private RecordButton recordButton;
     private TextView textView;
     private TextView currentDurationLabel;
     private TextView totalDurationLabel;
-    PermissionListener permissionlistener;
-    private static String mFileName = null;
     private StorageReference mStorageReference;
     private StorageReference filepathReference;
     private MediaPlayer m;
-    private SeekBar seekProgressBar;
+    private RangeSeekBar rangeSeekBar;
     private Handler mHandler = new Handler();
     private Utilities utils;
-    private VisualizerView visualizerView;
-    private Visualizer visualizer;
     private int length;
+    private ImageView playButton;
     private boolean isPaused;
     private String audioDownloadFilePath;
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            long currentDuration = m.getCurrentPosition();
+
+            // Displaying Total Duration time
+            // Displaying time completed playing
+            totalDurationLabel.setText("" + getTime((Integer) rangeSeekBar.getSelectedMaxValue()));
+            currentDurationLabel.setText("" + utils.milliSecondsToTimer(currentDuration));
+
+            // Running the thread after 100 milliseconds
+            mHandler.postDelayed(this, 100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,23 +94,91 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         setContentView(R.layout.activity_main);
 
         utils = new Utilities();
+        new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                .setTitleText("Get Started")
+                .setContentText("Now Hold the mic and speak")
+                .show();
 
         mRecorder = new MediaRecorder();
-        recordButton = findViewById(R.id.imageView);
+        recordButton = findViewById(R.id.recordBtn);
         textView = findViewById(R.id.textView);
-        seekProgressBar = findViewById(R.id.seekBar);
+        rangeSeekBar = findViewById(R.id.rangeSeekBar);
         currentDurationLabel = findViewById(R.id.textView2);
         totalDurationLabel = findViewById(R.id.textView3);
-        visualizerView = findViewById(R.id.visualizerview);
+        playButton = findViewById(R.id.button);
+        playButton.setImageResource(R.drawable.play);
         m = new MediaPlayer();
-        seekProgressBar.setOnSeekBarChangeListener(this); // Important
+        // Important
         m.setOnCompletionListener(this);
         audioDownloadFilePath = "";
-
+        rangeSeekBar.setRangeValues(0, m.getDuration());
+        rangeSeekBar.setNotifyWhileDragging(true);
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/new_audio.mp3";
 
+        Log.d(TAG, FirebaseStorage.getInstance().getReference() + "");
         mStorageReference = FirebaseStorage.getInstance().getReference();
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int duration = m.getDuration() / 1000;
+                currentDurationLabel.setText("00:00");
+                totalDurationLabel.setText(getTime(duration));
+                Log.d(TAG, mStorageReference.child("audio").child("new_audio.mp3").getDownloadUrl().toString());
+
+                if (isPaused) {
+                    m.seekTo(length);
+                    m.start();
+                    isPaused = false;
+                }
+
+                rangeSeekBar.setRangeValues(0, duration);
+                rangeSeekBar.setSelectedMinValue(0);
+                rangeSeekBar.setSelectedMaxValue(duration);
+                rangeSeekBar.setEnabled(true);
+                rangeSeekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
+                    @Override
+                    public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Integer minValue, Integer maxValue) {
+
+                        m.seekTo(minValue * 1000);
+                        currentDurationLabel.setText(getTime((Integer) bar.getSelectedMinValue()));
+                        totalDurationLabel.setText(getTime((Integer) bar.getSelectedMaxValue()));
+                    }
+                });
+                m.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                try {
+                    m.setDataSource(audioDownloadFilePath);
+                    m.prepare();
+                    m.start();
+
+                    if (isPaused) {
+                        m.seekTo(length);
+                        isPaused = false;
+                    }
+                    updateProgressBar();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                }
+                final Handler handler = new Handler();
+                handler.postDelayed(r = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if (m.getCurrentPosition() >= (Integer) rangeSeekBar.getSelectedMaxValue() * 1000) {
+                            m.seekTo((Integer) rangeSeekBar.getSelectedMinValue() * 1000);
+                        }
+                        handler.postDelayed(r, 100);
+                    }
+                }, 100);
+
+
+            }
+        });
 
         recordButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -131,10 +215,10 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
             Log.e(TAG, "start failed");
         }
 
-
     }
 
     public void stopRecording() {
+
 
         if (mRecorder != null) {
 
@@ -156,141 +240,48 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
 
         filepathReference = mStorageReference.child("audio").child("new_audio.mp3");
         Uri uri = Uri.fromFile(new File(mFileName));
-
+        new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                .setTitleText("Upload Successful")
+                .setContentText("Now click the play button!")
+                .show();
         filepathReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
 
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
                 Toast.makeText(MainActivity.this, "uploaded", Toast.LENGTH_SHORT).show();
-                Log.d(TAG,"Upload Successful");
+                Log.d(TAG, "Upload Successful");
+
                 textView.setText(R.string.upload_finish);
-
-
+                mStorageReference.child("audio").child("new_audio.mp3").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        audioDownloadFilePath = uri.toString();
+                        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+                    }
+                });
 
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(MainActivity.this, "failed to upload", Toast.LENGTH_SHORT).show();
-                Log.d(TAG,"Upload Failure");
+                Log.d(TAG, "Upload Failure");
             }
         });
     }
 
-    public void playAudio(View view) {
-
-        m = new MediaPlayer();
-
-        Log.d(TAG,mStorageReference.child("audio").child("new_audio.mp3").getDownloadUrl().toString());
-        mStorageReference.child("audio").child("new_audio.mp3").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                audioDownloadFilePath = uri.toString();
-            }
-        });
-
-        setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        setupVisualizerFxAndUI();
-        // Make sure the visualizer is enabled only when you actually want to
-        // receive data, and
-        // when it makes sense to receive data.
-        visualizer.setEnabled(true);
-        // When the stream ends, we don't need to collect any more data. We
-        // don't do this ins
-        // setupVisualizerFxAndUI because we likely want to have more,
-        // non-Visualizer related code
-        // in this callback.
-        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                visualizer.setEnabled(true);
-
-
-            }
-        });
-
-
-        m.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try {
-            m.setDataSource(audioDownloadFilePath);
-            m.prepare();
-            m.start();
-
-            if (isPaused) {
-                m.seekTo(length);
-                isPaused = false;
-
-            }
-            m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
-                    Toast.makeText(MainActivity.this, "finished", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            seekProgressBar.setProgress(0);
-            seekProgressBar.setMax(100);
-
-            // Updating progress bar
-            updateProgressBar();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+    private String getTime(int seconds) {
+        int hr = seconds / 3600;
+        int rem = seconds % 3600;
+        int mn = rem / 60;
+        int sec = rem % 60;
+        return String.format("%02d", hr) + ":" + String.format("%02d", mn) + ":" + String.format("%02d", sec);
     }
-
-    private void setupVisualizerFxAndUI() {
-        visualizer = new Visualizer(m.getAudioSessionId());
-        visualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
-        visualizer.setDataCaptureListener(
-                new Visualizer.OnDataCaptureListener() {
-                    public void onWaveFormDataCapture(Visualizer visualizer,
-                                                      byte[] bytes, int samplingRate) {
-                        visualizerView.updateVisualizer(bytes);
-                    }
-
-                    public void onFftDataCapture(Visualizer visualizer,
-                                                 byte[] bytes, int samplingRate) {
-                    }
-                }, Visualizer.getMaxCaptureRate() / 2, true, false);
-    }
-
 
     private void updateProgressBar() {
 
-        mHandler.postDelayed(mUpdateTimeTask, 100);
-
-
-    }
-
-    private Runnable mUpdateTimeTask = new Runnable() {
-        public void run() {
-            long totalDuration = m.getDuration();
-            long currentDuration = m.getCurrentPosition();
-
-            // Displaying Total Duration time
-            totalDurationLabel.setText("" + utils.milliSecondsToTimer(totalDuration));
-            // Displaying time completed playing
-            currentDurationLabel.setText("" + utils.milliSecondsToTimer(currentDuration));
-
-            // Updating progress bar
-            int progress = (utils.getProgressPercentage(currentDuration, totalDuration));
-            //Log.d("Progress", ""+progress);
-            seekProgressBar.setProgress(progress);
-
-            // Running the thread after 100 milliseconds
-            mHandler.postDelayed(this, 100);
-        }
-    };
-
-    public void pauseAudio(View view) {
-        if (m != null && m.isPlaying()) {
-            m.pause();
-            length = m.getCurrentPosition();
-            isPaused = true;
-        }
-
-
+        mHandler.postDelayed(mUpdateTimeTask, 200);
     }
 
     @Override
@@ -303,7 +294,6 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 
@@ -331,4 +321,13 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
     }
 
 
+    public void pauseAudio(View view) {
+        if (m != null && m.isPlaying()) {
+            m.pause();
+            length = m.getCurrentPosition();
+            isPaused = true;
+        }
+
+
+    }
 }
